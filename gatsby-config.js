c49728ce78937e9bf41fa747d7653ed5193175c5
Jan-Philipp <jpm3492@gmail.com>
2019-05-06 22:27:44 +0200

    offline support added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    'gatsby-plugin-offline',
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
