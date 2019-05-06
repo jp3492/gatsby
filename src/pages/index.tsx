c3a7e4ae66a3711ce0cab953c6d6fbbe33a407da
Jan-Philipp <jpm3492@gmail.com>
2019-05-06 22:05:54 +0200

    interactivitiy added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,42 @@
-import React from "react"
+import React, { useState, useMemo } from "react"
 import { Link } from "gatsby"
+import axios from 'axios'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Home</h1>
-    <Link to="/about">About</Link>
-    <Link to="/products">Products</Link>
+const IndexPage = () => {
+  const [users, setUsers] = useState([])
 
-  </Layout>
-)
+  useMemo(() => {
 
+  }, [useMemo])
+
+  const getUsers = () => axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(res => setUsers(res.data))
+    .catch(err => console.log(err))
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Home</h1>
+      <Link to="/about">About</Link>
+      <Link to="/products">Products</Link>
+      <ul>
+        {
+          users.map((user, i) => (
+            <div>
+              {
+                user.name
+              }
+            </div>
+          ))
+        }
+      </ul>
+      <button onClick={() => getUsers()}>
+        Get User
+      </button>
+    </Layout>
+  )
+}
 export default IndexPage
